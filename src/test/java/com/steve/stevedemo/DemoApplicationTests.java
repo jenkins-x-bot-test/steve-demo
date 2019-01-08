9eec67e560657b551b40e6058b0a0885cac9c1a2
Steve Arch <sarch@cloudbees.com>
2019-01-08 09:58:53 +0000

    Initial import

---
+++ src/test/java/com/steve/stevedemo/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.steve.stevedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
